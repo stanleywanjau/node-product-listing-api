66bd3ff23776514fc9c34550db671f97cd9fdbfe
stanley <stanleywanjau371@gmail.com>
2024-08-15 08:59:43 +0300

    console log the url

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require('./app')
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
-
+console.log("Running in port", `http://localhost:${ port }`)
 server.listen(port);
